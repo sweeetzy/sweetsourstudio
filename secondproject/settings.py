8281e7d006dbefb03b35862bf3e4e3586a34b4ad
github_sweeetzy <sweeetzy@naver.com>
2019-08-01 15:03:24 +0900

    first commit

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get( 'DJANGO_SECRET_KEY', 'q2bo0=a+g3=dybgrd*g&qfu(ck9mn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
